8ba8ee31b8928a2a17fd0b20ceb55e11c5cd8044
Rehane <abusayid693@gmail.com>
2022-01-13 10:08:45 +0530

    feat : login route

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,30 +1,65 @@
 import { Request, Response, NextFunction } from "express";
-import { User } from "../models/User"
+import { User } from "../models/User";
 
 exports.register = async (req: Request, res: Response, next: NextFunction) => {
-
   const { username, email, password } = req.body;
 
   try {
     const user = await User.create({
-      username, email, password
-    })
+      username,
+      email,
+      password,
+    });
 
     res.status(201).json({
       success: true,
-      data: user
-    })
-
+      data: user,
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
       error: error.message,
-    })
+    });
   }
 };
 
-exports.login = (req: Request, res: Response, next: NextFunction) => {
-  res.send("Login route");
+exports.login = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({
+      success: false,
+      error: "Provide all required fields",
+    });
+  }
+  try {
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        error: "User not found",
+      });
+    }
+    console.log("user b:",user)
+
+    const isMatch = await user.matchPasswords(password);
+    console.log("user :",user)
+
+    if (!isMatch) {
+      res.status(404).json({
+        success: false,
+        error: "User not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      token: "ewfetbtgerger",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      token: error.message,
+    });
+  }
 };
 
 exports.forgotPassword = (req: Request, res: Response, next: NextFunction) => {
--- models/User.ts
+++ models/User.ts
@@ -1,42 +1,49 @@
-import mongoose, { Model, Schema } from "mongoose"
-import bcrypt from "bcryptjs"
+import mongoose, { Model, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
 interface IUser {
-    username: string;
-    email: string;
-    password: string;
-    resetPasswordToken: string;
-    resetPasswordExpire: string;
+  username: string;
+  email: string;
+  password: string;
+  resetPasswordToken: string;
+  resetPasswordExpire: string;
 }
 
 const UserSchema: Schema = new Schema<IUser>({
-    username: {
-        type: String,
-        required: [true, "Username is required"]
-    },
-    email: {
-        type: String,
-        required: [true, "email is required"],
-        unique: true,
-        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please fill a valid email address']
-    },
-    password: {
-        type: String,
-        required: [true, "Please enter the password"],
-        minlength: 6,
-        select: false
-    },
-    resetPasswordToken: String,
-    resetPasswordExpire: Date
-})
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+    unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please fill a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter the password"],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
 
 UserSchema.pre("save", async function (next) {
-    if (!this.isModified["password"])
-        next()
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-})
+  if (!this.isModified("password")) next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-export const User = mongoose.model("User", UserSchema)
+UserSchema.methods.matchPasswords = async function (password) {
+  const self: any = this;
+  console.log("password :", password);
+  return await bcrypt.compare(password, self.password);
+};
 
+export const User = mongoose.model("User", UserSchema);
