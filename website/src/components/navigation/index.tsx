f2e43410dbdd54c77bd5796675c4e60d65c61288
Rehane <abusayid693@gmail.com>
2022-10-26 02:12:29 +0530

    feat: added site navigation

---
+++ website/src/components/navigation/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import * as S from './style';
+// @ts-ignore
+import icon from '../../images/icon.png';
+// @ts-ignore
+import githubIcon from '../../images/social/github.svg';
+
+import {ButtonLight} from '../button';
+
+export const Navigation = () => {
+  return (
+    <S.Container>
+      <article className="navigation-logo">
+        <img className="navigation-logo-icon" src={icon} />
+        <span className="navigation-logo-title">Gerome</span>
+      </article>
+
+      <article className="navigation-content">
+        <ul className="navigation-button-list">
+          <li className="navigation-button-list-item">
+            <ButtonLight>Log in</ButtonLight>
+          </li>
+        </ul>
+
+        <ul className="navigation-links-list">
+          <li className="navigation-links-list-item">
+            <img src={githubIcon} />
+          </li>
+        </ul>
+      </article>
+    </S.Container>
+  );
+};
---
+++ website/src/components/navigation/style.ts
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+export const Container = styled.nav`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  background-color: #0e1118;
+  margin: 0;
+  padding: 0 20px;
+  border-bottom: 1px solid #151b24;
+
+  .navigation-logo {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 10px;
+
+    .navigation-logo-icon {
+      width: 25px;
+    }
+
+    .navigation-logo-title {
+      color: white;
+      font-size: 20px;
+    }
+  }
+
+  .navigation-content {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 15px;
+  }
+
+  .navigation-button-list {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+  }
+
+  .navigation-links-list {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    border-left: 2px solid #151b24;
+    padding-left: 10px;
+
+    .navigation-links-list-item {
+      cursor: pointer;
+      color: red;
+
+      & img {
+        width: 25px;
+        height: 25px;
+        color: red;
+      }
+    }
+  }
+`;
