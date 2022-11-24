6966ae3a4f4b10a4ecb5ec6db0bc63877462fd4c
Rehane <abusayid693@gmail.com>
2022-11-24 20:43:01 +0530

    fix: form fixes

--- client/pages/signup/FormModal/validationSchema.ts
+++ client/pages/signup/FormModal/validationSchema.ts
@@ -3,7 +3,12 @@ import * as Yup from 'yup';
 export default Yup.object().shape({
   username: Yup.string().min(5, 'Too short').max(15, 'Too long').required('Required'),
   email: Yup.string().email('Invalid email').required('Required'),
-  password: Yup.string().required('Required'),
+  password: Yup.string()
+    .required('Required')
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character'
+    ),
   confirmPassword: Yup.string()
     .required('Required')
     .oneOf([Yup.ref('password'), null], 'Passwords must match')
--- client/pages/signup/index.tsx
+++ client/pages/signup/index.tsx
@@ -2,10 +2,14 @@ import {faUser} from '@fortawesome/free-solid-svg-icons';
 import {Form, Formik, FormikHelpers} from 'formik';
 import _ from 'lodash';
 import Link from 'next/link';
+import {useRouter} from 'next/router';
+import {useEffect} from 'react';
+import {useCookies} from 'react-cookie';
 import {AuthServices} from '../../../packages/gerome-api';
 import {ButtonDark, ButtonLight, FieldInput, PasswordInput} from '../../components';
 import {useToast} from '../../hooks/useToast';
 import {ErrorFormat} from '../../util';
+import {COOKIE} from '../../util/cookie';
 import type initialValue from './FormModal/initialValues';
 import initialValues from './FormModal/initialValues';
 import validationSchema from './FormModal/validationSchema';
@@ -13,19 +17,30 @@ import validationSchema from './FormModal/validationSchema';
 const Index = () => {
   const api = new AuthServices();
   const toast = useToast();
+  const [__, ___, removeCookie] = useCookies([COOKIE]);
+  const router = useRouter();
 
   const __hanldeFormSubmit = async (values: typeof initialValue, actions: FormikHelpers<typeof initialValue>) => {
     try {
       await api.register(_.omit(values, 'confirmPassword'));
+      toast('SUCCESS', 'User successfully registered');
+      router.push('/login');
     } catch (error: any) {
       if (error.response.status !== 500) {
         const errors = error.response.data.errors;
         actions.setErrors(ErrorFormat(errors));
       } else {
-        toast('ERROR', 'Login error');
+        toast('ERROR', 'Sign up error');
       }
     }
   };
+
+  /**
+   * Note: forcefully clear all session cookies
+   */
+  useEffect(() => {
+    removeCookie(COOKIE, {path: '/'});
+  }, []);
 
   return (
     <div className="border-2 border-slate-300 rounded max-w-lg display-block m-auto p-4">
@@ -41,7 +56,9 @@ const Index = () => {
                 <FieldInput name="email" placeholder="Email" />
                 <PasswordInput name="password" placeholder="Password" />
                 <PasswordInput name="confirmPassword" placeholder="Confirm Password" />
-                <ButtonDark isLoading={isSubmitting}>Sign Up</ButtonDark>
+                <ButtonDark isLoading={isSubmitting} type="submit">
+                  Sign Up
+                </ButtonDark>
                 <p className="pt-2 text-black text-xs opacity-7">By signing up, you agree to the Terms of Service and Privacy Policy</p>
                 <div className="py-2 flex flex-row gap-2 items-center text-sm text-gray-700">
                   <hr className="w-full" />
